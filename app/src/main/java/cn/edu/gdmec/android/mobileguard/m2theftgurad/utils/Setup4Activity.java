9ca82ef21129d5ed17b5b759da59f1977d7997b2
forevermistery <1258234227@qq.com>
2017-10-24 21:26:26 +0800

    Rejected commit: Default

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftgurad/utils/Setup4Activity.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftgurad/utils/Setup4Activity.java
@@ -16,44 +16,49 @@ import cn.edu.gdmec.android.mobileguard.R;
 public class Setup4Activity extends BaseSetUpActivity {
     private TextView mStatusTV;
     private ToggleButton mToggleButton;
-
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setup_4);
+        //设置第4个小圆点的颜色
         ((RadioButton)findViewById(R.id.rb_four)).setChecked(true);
         initView();
     }
+
     private void initView(){
         ((RadioButton)findViewById(R.id.rb_four)).setChecked(true);
-        mStatusTV=(TextView)findViewById(R.id.tv_setup4_status);
-        mToggleButton=(ToggleButton)findViewById(R.id.togglebtn_securityfunction);
+        mStatusTV = (TextView) findViewById(R.id.tv_setup4_status);
+        mToggleButton = (ToggleButton) findViewById(R.id.togglebtn_securityfunction);
         mToggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+
             @Override
-            public void onCheckedChanged(CompoundButton buttonView,boolean isChecked){
-                if (isChecked){
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
                     mStatusTV.setText("防盗保护已经开启");
-                }else{
+                }else {
                     mStatusTV.setText("防盗保护没有开启");
                 }
-                SharedPreferences.Editor editor=sp.edit();
-                editor.putBoolean("portecting",isChecked);
+                SharedPreferences.Editor editor = sp.edit();
+                editor.putBoolean("protecting",isChecked);
                 editor.commit();
             }
         });
-        boolean protecting=sp.getBoolean("protecting",true);
+
+        boolean protecting = sp.getBoolean("protecting",true);
         if (protecting){
             mStatusTV.setText("防盗保护已经开启");
             mToggleButton.setChecked(true);
-        }else{
+        }else {
             mStatusTV.setText("防盗保护没有开启");
             mToggleButton.setChecked(false);
         }
     }
+
     @Override
     public void showNext(){
-        SharedPreferences.Editor editor=sp.edit();
-        editor.putBoolean("isSetup",true);
+        //跳转至防盗保护页面
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putBoolean("isSetUp",true);
         editor.commit();
         startActivityAndFinishSelf(LostFindActivity.class);
     }
