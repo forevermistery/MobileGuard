1abf5fd3cbbad359ee8dd3b9323059fd9b186547
forevermistery <1258234227@qq.com>
2017-11-27 16:27:38 +0800

    m8

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m8trafficmonitor/db/TrafficOpenHelper.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m8trafficmonitor/db/TrafficOpenHelper.java
@@ -5,25 +5,32 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 /**
- * Created by Administrator on 2017/11/26 0026.
+ * Created by zhh on 2017/11/27.
  */
 
-public class TrafficOpenHelper extends SQLiteOpenHelper {
-    private static final String DB_NAME="traffic.db";
-    private static final String TABLE_NAME="traffic";
-    /**流量**/
-    private final static String GPRS="gprs";
-    private final static String TIME="date";
-    public TrafficOpenHelper(Context context){
-        super(context,DB_NAME,null,1);
-    }
-    @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase)
-    {
-        sqLiteDatabase.execSQL("create table "+TABLE_NAME+"(id integer primary key autoincrement,"+GPRS+" varchar(255),"+TIME+" datetime)");
-    }
-   @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase,int i,int i1){
 
-   }
+public class TrafficOpenHelper extends SQLiteOpenHelper {
+    private static final String DB_NAME = "traffic.db";
+    private static final String TABLE_NAME = "traffic";
+    /** 流量 */
+    private final static String GPRS = "gprs";
+    private final static String TIME = "date";
+
+    public TrafficOpenHelper(Context context) {
+        super(context, DB_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table " + TABLE_NAME
+                + "(id integer primary key autoincrement," + GPRS
+                + " varchar(255)," + TIME + " datetime)");
+    }
+
+    //     public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
 }
--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m8trafficmonitor/reciever/BootCompleteReciever.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m8trafficmonitor/reciever/BootCompleteReciever.java
@@ -9,17 +9,14 @@ import cn.edu.gdmec.android.mobileguard.m8trafficmonitor.service.TrafficMonitori
 import cn.edu.gdmec.android.mobileguard.m8trafficmonitor.utils.SystemInfoUtils;
 
 /**
- * Created by Administrator on 2017/11/26 0026.
+ * Created by zhh on 2017/11/26.
  */
 
-public class BootCompleteReciever extends BroadcastReceiver {
+public class BootCompleteReciever extends BroadcastReceiver{
     @Override
-    public void onReceive(Context context, Intent intent){
-        //开机广播
-        //判断流量监控服务是否开启,如果没有开启则开启
-        if (!SystemInfoUtils.isServiceRunning(context,
-                "cn.edu.gdmec.android.mobileguard.m8trafficmonitor.service.TrafficMonitoringService")){
-            //开启服务
+    public void onReceive(Context context, Intent intent) {
+        if(!SystemInfoUtils.isServiceRunning(context,
+                "cn.edu.gdmec.android.monileguard.m8trafficmonitor.service.TrafficMonitoringService")){
             Log.d("traffic service","turn on");
             context.startService(new Intent(context, TrafficMonitoringService.class));
         }
