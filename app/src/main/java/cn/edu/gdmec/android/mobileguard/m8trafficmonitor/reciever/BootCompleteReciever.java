17826970b6e37b4887ac93beef6f54127a289701
forevermistery <1258234227@qq.com>
2017-11-27 20:05:59 +0800

    m8

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m8trafficmonitor/reciever/BootCompleteReciever.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m8trafficmonitor/reciever/BootCompleteReciever.java
@@ -9,14 +9,16 @@ import cn.edu.gdmec.android.mobileguard.m8trafficmonitor.service.TrafficMonitori
 import cn.edu.gdmec.android.mobileguard.m8trafficmonitor.utils.SystemInfoUtils;
 
 /**
- * Created by lenovo on 2017/11/26.
+ * Created by zhh on 2017/11/27.
  */
 
 public class BootCompleteReciever extends BroadcastReceiver{
     @Override
-    public void onReceive(Context context, Intent intent) {
-        if(!SystemInfoUtils.isServiceRunning(context,
-                "cn.edu.gdmec.android.monileguard.m8trafficmonitor.service.TrafficMonitoringService")){
+    public void onReceive(Context context, Intent intent){
+
+        if (!SystemInfoUtils.isServiceRunning(context,
+                "cn.edu.gdmec.android.mobileguard.m8trafficmonitor.service.TrafficMonitoringService")){
+
             Log.d("traffic service","turn on");
             context.startService(new Intent(context, TrafficMonitoringService.class));
         }
--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m8trafficmonitor/service/TrafficMonitoringService.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m8trafficmonitor/service/TrafficMonitoringService.java
@@ -13,11 +13,10 @@ import java.util.Date;
 import cn.edu.gdmec.android.mobileguard.m8trafficmonitor.db.dao.TrafficDao;
 
 /**
- * Created by Jack on 2017/11/27.
+ * Created by zhh on 2017/9/20.
  */
 
-
-public class TrafficMonitoringService extends Service {
+public class TrafficMonitoringService extends Service{
     private long mOldRxBytes;
     private long mOldTxBytes;
     private TrafficDao dao;
@@ -26,11 +25,11 @@ public class TrafficMonitoringService extends Service {
     boolean flag = true;
 
     @Override
-    public IBinder onBind(Intent intent) {
+    public IBinder onBind(Intent intent){
         return null;
     }
     @Override
-    public void onCreate() {
+    public void onCreate(){
         super.onCreate();
         mOldRxBytes = TrafficStats.getMobileRxBytes();
         mOldTxBytes = TrafficStats.getMobileTxBytes();
@@ -39,52 +38,47 @@ public class TrafficMonitoringService extends Service {
         mThread.start();
     }
 
-    private Thread mThread = new Thread() {
-        public void run() {
-            while (flag) {
+    private Thread mThread = new Thread(){
+        public void run(){
+            while (flag){
                 try {
                     Thread.sleep(2000 * 60);
-                } catch (InterruptedException e) {
+                }catch (InterruptedException e){
                     e.printStackTrace();
                 }
                 updateTodayGPRS();
             }
         }
-        private void updateTodayGPRS() {
-            // 获取已经使用了的流量
+        private void updateTodayGPRS(){
             usedFlow = mSp.getLong("usedflow", 0);
             Date date = new Date();
-
-            Calendar calendar = Calendar.getInstance(); // 得到日历
-
-            calendar.setTime(date);// 把当前时间赋给日历
-
-            if (calendar.DAY_OF_MONTH == 1 & calendar.HOUR_OF_DAY == 0
-                    & calendar.MINUTE < 1 & calendar.SECOND < 30) {
-                usedFlow = 0;
-            }
-            SimpleDateFormat sdf = new SimpleDateFormat ("yyyy-MM-dd");
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(date);
+            if (calendar.DAY_OF_MONTH == 1 & calendar.HOUR_OF_DAY ==0)
+                if(calendar.DAY_OF_MONTH == 1 & calendar.HOUR_OF_DAY ==0
+                        & calendar.MINUTE < 1 & calendar.SECOND < 30){
+                    usedFlow = 0;
+                }
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
             String dataString = sdf.format(date);
-            long moblieGPRS = dao.getMoblieGPRS(dataString);
+            long mobileGPRS = dao.getMoblieGPRS(dataString);
             long mobileRxBytes = TrafficStats.getMobileRxBytes();
             long mobileTxBytes = TrafficStats.getMobileTxBytes();
 
-            // 新产生的流量
             long newGprs = (mobileRxBytes + mobileTxBytes) - mOldRxBytes
                     - mOldTxBytes;
             mOldRxBytes = mobileRxBytes;
             mOldTxBytes = mobileTxBytes;
-            if (newGprs < 0) {
-                // 网络切换过
+            if (newGprs < 0){
                 newGprs = mobileRxBytes + mobileTxBytes;
             }
-            if (moblieGPRS == -1) {
+            if (mobileGPRS == -1) {
                 dao.insertTodayGPRS(newGprs);
-            } else {
-                if (moblieGPRS < 0) {
-                    moblieGPRS = 0;
+            }else{
+                if (mobileGPRS < 0){
+                    mobileGPRS = 0;
                 }
-                dao.UpdateTodayGPRS(moblieGPRS + newGprs);
+                dao.UpdataTodayGPRS(mobileGPRS + newGprs);
             }
             usedFlow = usedFlow + newGprs;
             SharedPreferences.Editor edit = mSp.edit();
@@ -92,10 +86,9 @@ public class TrafficMonitoringService extends Service {
             edit.commit();
         };
     };
-
     @Override
-    public void onDestroy() {
-        if (mThread != null & !mThread.interrupted()) {
+    public void onDestroy(){
+        if (mThread != null & !mThread.interrupted()){
             flag = false;
             mThread.interrupt();
             mThread = null;
@@ -103,4 +96,3 @@ public class TrafficMonitoringService extends Service {
         super.onDestroy();
     }
 }
-
