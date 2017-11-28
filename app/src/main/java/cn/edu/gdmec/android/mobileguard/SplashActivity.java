b9e1c653ad02737259ef2958e77c168d8716b304
forevermistery <1258234227@qq.com>
2017-11-28 21:26:44 +0800

    m5云查杀

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/SplashActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/SplashActivity.java
@@ -1,16 +1,14 @@
 package cn.edu.gdmec.android.mobileguard;
 
-
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
-        import android.os.Bundle;
-        import android.widget.TextView;
+import android.os.Bundle;
+import android.view.Window;
+import android.widget.TextView;
 
-
-        import cn.edu.gdmec.android.mobileguard.m1home.HomeActivity;
-        import cn.edu.gdmec.android.mobileguard.m1home.utils.MyUtils;
+import cn.edu.gdmec.android.mobileguard.m1home.HomeActivity;
+import cn.edu.gdmec.android.mobileguard.m1home.utils.MyUtils;
 import cn.edu.gdmec.android.mobileguard.m1home.utils.VersionUpdateUtils;
-
 
 public class SplashActivity extends AppCompatActivity {
     private TextView mTvVersion;
@@ -18,13 +16,17 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //11.21
+        requestWindowFeature( Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();
         mVersion = MyUtils.getVersion(getApplicationContext());
-        mTvVersion = (TextView) findViewById(R.id.tv_splash_version);
-        mTvVersion.setText("版本号:"+mVersion);
-        VersionUpdateUtils.DownloadCallback downloadCallback=new VersionUpdateUtils.DownloadCallback(){
+        mTvVersion = (TextView)findViewById(R.id.tv_splash_version);
+        mTvVersion.setText("版本号："+mVersion);
+        //更新apk
+        VersionUpdateUtils.DownloadCallback downloadCallback = new VersionUpdateUtils.DownloadCallback() {
             @Override
-            public void afterDownload(String filename){
+            public void afterDownload(String filename) {
                 MyUtils.installApk(SplashActivity.this,filename);
             }
         };
@@ -32,10 +34,24 @@ public class SplashActivity extends AppCompatActivity {
         new Thread(){
             @Override
             public void run(){
+                super.run();
                 versionUpdateUtils.getCloudVersion("http://android2017.duapp.com/updateinfo.html");
+
             }
         }.start();
-        startActivity(new Intent(this, HomeActivity.class));
-        finish();
+
+        //final VersionUpdateUtils versionUpdateUtils = new VersionUpdateUtils(mVersion,SplashActivity.this);
+/*        new Thread(){
+            @Override
+            public void run(){
+                super.run();
+                versionUpdateUtils.getCloudVersion();
+                versionUpdateUtils.getCloudVersion("http://android2017.duapp.com/updateinfo.html");
+            }
+        }.start();*/
+
+        //老师
+       /* startActivity(new Intent (this, HomeActivity.class));
+        finish();*/
     }
 }
